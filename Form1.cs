560f2b6aad6fce9de25625244ac1102586a03620
ShakingCan <kamura097@gmail.com>
2026-03-14 08:59:32 +0800

    Populate monthbox with months from ArrayList
    Added a list of months to monthbox from an ArrayList.

--- Form1.cs
+++ Form1.cs
@@ -70,6 +70,26 @@ namespace StudentRegistrationApplication
             {
                 comboBox1.Items.Add(cose);
             }
+            ArrayList months = new ArrayList();
+months.Add("January");
+months.Add("February");
+months.Add("March");
+months.Add("April");
+months.Add("May");
+months.Add("June");
+months.Add("July");
+months.Add("August");
+months.Add("September");
+months.Add("October");
+months.Add("November");
+months.Add("December");
+foreach (String month in months)
+{
+    monthbox.Items.Add(month);
+}
+
+
+            
         }
 
       
